1ba943be0a48d7a619975e4dc73d28f39832a433
TheLukCraft <luk1997r@o2.pl>
2023-07-04 18:39:35 +0200

    retrieves values from the settings.json file

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 using ZipExtractor;
 
 string settingsPath = "settings.json";
+string output = string.Empty;
 
 Settings settings = new Settings();
+Settings deserializedData = new Settings();
 
 //Creates a new settings.json file with default values in case settings.json does not exist.
 if (!File.Exists(settingsPath))
@@ -12,5 +14,7 @@ if (!File.Exists(settingsPath))
     settings.FilePath = "exact path to the file";
     settings.FileExtension = ".zip";
 
-    string output = JsonConvert.SerializeObject(settings);
+    output = JsonConvert.SerializeObject(settings);
 }
+
+deserializedData = JsonConvert.DeserializeObject<Settings>(output);
