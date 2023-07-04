5d8f15b753e37629d1c0af5b3016c3cd73efcef6
TheLukCraft <luk1997r@o2.pl>
2023-07-04 18:36:07 +0200

    Serializing json data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using ZipExtractor;
+
+string settingsPath = "settings.json";
+
+Settings settings = new Settings();
+
+//Creates a new settings.json file with default values in case settings.json does not exist.
+if (!File.Exists(settingsPath))
+{
+    settings.FileName = "folder name after extracting";
+    settings.FilePath = "exact path to the file";
+    settings.FileExtension = ".zip";
+
+    string output = JsonConvert.SerializeObject(settings);
+}
---
+++ Settings.cs
@@ -0,0 +1,9 @@
+﻿namespace ZipExtractor
+{
+    public class Settings
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string FileExtension { get; set; }
+    }
+}
